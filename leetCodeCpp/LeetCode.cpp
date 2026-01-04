20692a3af98b2420ef10c8b2a8877ab2f7b64afc
Tunahan AKYOL <tnakyol@hotmail.com>
2026-01-04 19:49:38 +0300

    added mergedTwoSortedLists and validParenthesis

---
+++ leetCodeCpp/LeetCode.cpp
@@ -0,0 +1,44 @@
+﻿// LeetCode.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <string>
+
+int strStr(std::string haystack, std::string needle)
+{
+    std::cout << haystack.size()- needle.size();
+    
+    if (haystack.size() >= needle.size())
+    {
+        for (int i = 0; i <= haystack.size() - needle.size(); ++i) {
+
+            std::string substr = haystack.substr(i, needle.size());
+            if (substr == needle)
+            {
+                std::cout << i;
+                return i;
+            }
+
+
+        }
+    }
+    std::cout << -1;
+    return -1;
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
+
+
+int main()
+{
+    strStr("aaa", "aaaa");
+}
